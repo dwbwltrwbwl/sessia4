d7b6e67de7e89ce49d427143129bcd81288abb20
dwbwltrwbwl <tyumenelizaveta@yandex.ru>
2025-09-16 11:57:06 +0300

    Добавьте файлы проекта.

---
+++ sessia4/ApplicationData/AppConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sessia4.ApplicationData
+{
+    internal class AppConnect
+    {
+        public static sessia4Entities model01;
+    }
+}
